00286cba51573dce31f8c2751edf44fff697ac1a
Reannu <reannub@gmail.com>
2024-03-01 15:09:37 +0800

    Add header Section
    - Added the top row buttons
    - Added simple styling for the header and its buttons

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
-function App() {}
+// import Header from "./header.jsx";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="header">
+      {/* <h1>Header</h1> */}
+      <div className="optionRow">
+        <button className="optionDiv">Home</button>
+        <button className="optionDiv">About Us</button>
+        <button className="optionDiv">Branches</button>
+        <button className="optionDiv">Pricing</button>
+      </div>
+    </div>
+    // <Header />;
+  );
+};
 
 export default App;
